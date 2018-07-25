8b00fd4599a70d4188988bca86cb0082d09421ba
dang <>
2018-07-25 10:55:00 +0800

    分文件夹存储

---
+++ react1/10/src/js/index.js
@@ -0,0 +1,20 @@
+var React = require('react')
+var ReactDom = require('react-dom')
+import CompometHeader from '../components/header'
+import CompometBody from '../components/body'
+import CompometFooter from '../components/footer'
+
+class Index extends React.Component {
+    render() {
+        return (
+            <div>
+                <CompometHeader></CompometHeader>
+                <CompometBody userId={123456}></CompometBody>
+                <CompometFooter></CompometFooter>
+            </div>
+        )
+    }
+}
+
+ReactDom.render(
+    <Index/>, document.getElementById('example'))
